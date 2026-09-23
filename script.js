2f96bc79f4b6f0a616c9f3b072ce7f2b9d9b8562
cnirmalaj <c.nirmala.j@icloud.com>
2026-09-24 00:18:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function suivant() {
+    document.querySelector(".page").style.display = "none";
+    document.querySelector(".page2").style.display = "block";
+
+}
+function dernierePage() {
+    document.querySelector(".page2").style.display = "none";
+    document.querySelector(".page3").style.display = "block";
+}
